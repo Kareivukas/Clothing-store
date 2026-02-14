e423b1c944c7278ab8dbc1f234c5cd66ca9fc7b4
Kareivukas <aurelija37c@gmail.com>
2026-02-14 12:24:11 +0200

    Created separate Footer.php file

---
+++ Footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+            <p>Where Snowflakes Meet Blueprints.</p>
+            <p style="margin-top: 10px;"> Frost & Form - Finnish winter wear brand since 2025</p>
+    </footer>
+    
+</body>
+</html>
